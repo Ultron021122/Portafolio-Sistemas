537b2b6e91540de00ca96e27ef7f261dbcad663a
Sebastian Martinez Lopez <sebastian.martinez7643@alumnos.udg.mx>
2023-05-10 23:54:53 -0500

    version 2.3.1

--- src/views/Activities.vue
+++ src/views/Activities.vue
@@ -7,47 +7,71 @@ import AccordionBottom from '../components/AccordionBottom.vue';
 <template>
     <Navbar></Navbar>
     <main class="container mx-auto">
-        <h1 class="text-4xl font-extrabold dark:text-white">Uso, Adaptación y Explotación de Sistemas Operativos</h1>
-        <div>
-            <h2 class="text-3xl font-bold dark:text-white">Unidad 1<small
-                    class="ml-2 font-semibold text-gray-500 dark:text-gray-400">Introducción a los sistemas
+        <h1 class="my-5 text-4xl text-center font-extrabold dark:text-white">Uso, Adaptación y Explotación de Sistemas
+            Operativos</h1>
+        <div class="mt-10">
+            <h2 class="text-center text-2xl md:text-3xl font-bold dark:text-white">Unidad temática 1.<small
+                    class="ml-2 flex justify-center md:inline-block font-semibold text-gray-500 dark:text-gray-400">Introducción
+                    a los sistemas
                     operativos</small></h2>
         </div>
-        <AccordionTop>
-            <template v-slot:title>
-                What is Flowbite?
-            </template>
-            <template v-slot:content>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia,
-                porro. Non a excepturi, voluptatibus ipsam magnam, eligendi,
-                accusantium ipsa quae quis praesentium voluptate saepe ullam sint ea
-                itaque consectetur impedit?
-            </template>
-        </AccordionTop>
-        <AccordionCenter>
-            <template v-slot:title>
-                What is Flowbite?
-            </template>
-            <template v-slot:content>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia,
-                porro. Non a excepturi, voluptatibus ipsam magnam, eligendi,
-                accusantium ipsa quae quis praesentium voluptate saepe ullam sint ea
-                itaque consectetur impedit?
-            </template>
-        </AccordionCenter>
-        <AccordionBottom>
-            <template v-slot:title>
-                What is Flowbite?
-            </template>
-            <template v-slot:content>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia,
-                porro. Non a excepturi, voluptatibus ipsam magnam, eligendi,
-                accusantium ipsa quae quis praesentium voluptate saepe ullam sint ea
-                itaque consectetur impedit?
-            </template>
-        </AccordionBottom>
+        <div class="md:w-4/5 mx-auto">
+            <AccordionTop class="mt-4">
+                <template v-slot:title>
+                    Objetivo de la unidad temática
+                </template>
+                <template v-slot:content>
+                    El estudiante explica el concepto de sistema operativo a partir
+                    de los diferentes enfoques y modos de operación a fin de reconocer
+                    su importancia en el sistema de computo.
+                </template>
+            </AccordionTop>
+            <AccordionCenter>
+                <template v-slot:title>
+                    Introducción
+                </template>
+                <template v-slot:content>
+                    En esta unidad se conocerán las características de los sistemas
+                    operativos así como sus funciones y objetivos.
+                </template>
+            </AccordionCenter>
+            <AccordionBottom>
+                <template v-slot:title>
+                    Actividades
+                </template>
+                <template v-slot:content>
+                    Durante esta unidad, nos enfocamos en alcanzar el objetivo establecido por nuestra profesora. Participamos 
+                    en diversas actividades que nos permitieron adquirir los conocimientos y habilidades necesarios. A 
+                    continuación, se detallan las actividades que realizamos:
+                    <ul class="pl-5 mt-2 text-gray-500 list-disc dark:text-gray-400">
+                        <li><a href="#actividad1" @click="scrollToElement"
+                                class="text-blue-600 dark:text-blue-500 hover:underline">Actividad de Aprendizaje 0</a></li>
+                        <li><a href="#actividad2" @click="scrollToElement"
+                                class="text-blue-600 dark:text-blue-500 hover:underline">Actividad de Aprendizaje 1</a></li>
+                        <li><a href="#actividad3" @click="scrollToElement"
+                                class="text-blue-600 dark:text-blue-500 hover:underline">Actividad de Aprendizaje 2</a></li>
+                        <li><a href="#actividad4" @click="scrollToElement"
+                                class="text-blue-600 dark:text-blue-500 hover:underline">Actividad de Aprendizaje 3</a></li>
+                    </ul>
+                </template>
+            </AccordionBottom>
+        </div>
     </main>
 </template>
+<script>
+export default {
+    methods: {
+        scrollToElement(event) {
+            event.preventDefault();
+            const targetId = event.target.getAttribute("href");
+            const targetElement = document.querySelector(targetId);
+            if (targetElement) {
+                targetElement.scrollIntoView({ behavior: "smooth" });
+            }
+        }
+    }
+};
+</script>
 <style scoped>
 @media (min-width: 480px) {
     .container {
